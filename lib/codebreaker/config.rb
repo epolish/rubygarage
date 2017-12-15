2528a9b3867079c48081279665a0dc0a028ccf5e
Eugene <polish@i.ua>
2017-12-15 14:04:35 +0200

    changed application folder

--- lib/codebreaker/config.rb
+++ lib/codebreaker/config.rb
@@ -2,6 +2,6 @@ module Codebreaker
   module Config
     LOCALE_FOLDER = 'locale'
     USER_DATA_FOLDER = 'user_data'
-    APPLICATION_FOLDER = 'application'
+    APPLICATION_FOLDER = 'codebreaker'
   end
 end
